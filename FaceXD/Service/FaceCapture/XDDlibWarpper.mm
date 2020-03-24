cd1b56c6c7d0054802b9798fde3b22c11c163eec
eric3u <eric3u@outlook.com>
2020-03-25 00:52:57 +0800

    Feat: 完成卡尔曼滤波

--- FaceXD/Service/FaceCapture/XDDlibWarpper.mm
+++ FaceXD/Service/FaceCapture/XDDlibWarpper.mm
@@ -14,6 +14,7 @@
 #import <opencv2/imgproc/types_c.h>
 #import <opencv2/highgui.hpp>
 #import <opencv2/imgproc.hpp>
+#import <opencv2/video/tracking.hpp>
 #import "XDDlibWarpper.h"
 
 extern UIImage* MatToUIImage(const cv::Mat& image);
@@ -34,8 +35,12 @@ extern void UIImageToMat(const UIImage* image,
 }
 @end
 
+#define kDlibFaceLandmarkCount (68)
 @interface XDDlibShapePredictor () {
     dlib::shape_predictor _predictor;
+    cv::KalmanFilter _kalmanFilterX;
+    cv::KalmanFilter _kalmanFilterY;
+    cv::Mat _controlVector; // U
 }
 @end;
 @implementation XDDlibShapePredictor
@@ -43,9 +48,34 @@ extern void UIImageToMat(const UIImage* image,
     self = [super init];
     if (self) {
         _predictor = predictor;
+        
+        double Q = 1; // 越大，对模型信任度越低，偏向于原始数据
+        double R = 10; // 越大，于观测数据信任越低
+        
+        /// kalmanFilterX
+        _kalmanFilterX.init(kDlibFaceLandmarkCount, kDlibFaceLandmarkCount, kDlibFaceLandmarkCount);
+        cv::setIdentity(_kalmanFilterX.statePost, cv::Scalar::all(0)); // x
+        cv::setIdentity(_kalmanFilterX.measurementMatrix); // H
+        cv::setIdentity(_kalmanFilterX.controlMatrix); // B
+        cv::setIdentity(_kalmanFilterX.transitionMatrix); // F
+        cv::setIdentity(_kalmanFilterX.errorCovPost); // P
+        cv::setIdentity(_kalmanFilterX.processNoiseCov, cv::Scalar::all(Q)); // Q
+        cv::setIdentity(_kalmanFilterX.measurementNoiseCov, cv::Scalar::all(R)); // R
+        /// kalmanFilterY
+        _kalmanFilterY.init(kDlibFaceLandmarkCount, kDlibFaceLandmarkCount, kDlibFaceLandmarkCount);
+        cv::setIdentity(_kalmanFilterY.statePost, cv::Scalar::all(0)); // x
+        cv::setIdentity(_kalmanFilterY.measurementMatrix); // H
+        cv::setIdentity(_kalmanFilterY.controlMatrix); // B
+        cv::setIdentity(_kalmanFilterY.transitionMatrix); // F
+        cv::setIdentity(_kalmanFilterY.errorCovPost); // P
+        cv::setIdentity(_kalmanFilterY.processNoiseCov, cv::Scalar::all(Q)); // Q
+        cv::setIdentity(_kalmanFilterY.measurementNoiseCov, cv::Scalar::all(R)); // R
+        
+        _controlVector = cv::Mat::zeros(kDlibFaceLandmarkCount, 1, CV_32F);
     }
     return self;
 }
+
 - (void)loadModelWithPath:(NSString *)path {
     dlib::deserialize([path cStringUsingEncoding:NSUTF8StringEncoding]) >> _predictor;
 }
@@ -67,21 +97,34 @@ extern void UIImageToMat(const UIImage* image,
     
     // for every detected face
     NSMutableArray *array = [[NSMutableArray alloc] init];
-    for (unsigned long j = 0; j < convertedRectangles.size(); ++j)
-    {
-        dlib::rectangle oneFaceRect = convertedRectangles[j];
-        
-        // detect all landmarks
-        dlib::full_object_detection shape = _predictor(img, oneFaceRect);
-        
-        // and draw them into the image (samplebuffer)
-        for (unsigned long k = 0; k < shape.num_parts(); k++) {
-            dlib::point p = shape.part(k);
-            CGPoint point = CGPointMake(p.x(), p.y());
-            cv::rectangle(mat, cv::Rect(point.x,point.y,4,4), cv::Scalar(255,0,0,255),-1);
-            NSValue *v = [NSValue valueWithCGPoint:point];
-            [array addObject:v];
-        }
+    cv::Mat zX = cv::Mat::zeros(kDlibFaceLandmarkCount, 1, CV_32F);
+    cv::Mat zY = cv::Mat::zeros(kDlibFaceLandmarkCount, 1, CV_32F);
+    
+    dlib::rectangle oneFaceRect = convertedRectangles[0];
+    // detect all landmarks
+    dlib::full_object_detection shape = _predictor(img, oneFaceRect);
+    
+    // and draw them into the image (samplebuffer)
+    for (unsigned long k = 0; k < shape.num_parts(); k++) {
+        dlib::point p = shape.part(k);
+        zX.at<float>((int)k, 0) = (float)p.x();
+        zY.at<float>((int)k, 0) = (float)p.y();
+    }
+    
+    
+    // Kalman update
+    _kalmanFilterX.predict(_controlVector);
+    cv::Mat stateX = _kalmanFilterX.correct(zX);
+    
+    _kalmanFilterY.predict(_controlVector);
+    cv::Mat stateY = _kalmanFilterY.correct(zY);
+    
+    // After update
+    for (int i = 0; i < kDlibFaceLandmarkCount; ++i) {
+        CGPoint point = CGPointMake(stateX.at<float>(i, 0), stateY.at<float>(i, 0));
+        cv::rectangle(mat, cv::Rect(point.x,point.y,4,4), cv::Scalar(255,0,0,255),-1);
+        NSValue *v = [NSValue valueWithCGPoint:point];
+        [array addObject:v];
     }
     
     CVPixelBufferUnlockBaseAddress(pixelBuffer,0);
