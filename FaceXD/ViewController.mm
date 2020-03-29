a8f22ef26a370349375b6df6d14c4ad7bd06a50c
eric3u <eric3u@outlook.com>
2020-03-29 20:51:48 +0800

    Chore: 整理依赖 Test: 去掉调整调试代码

--- FaceXD/ViewController.mm
+++ FaceXD/ViewController.mm
@@ -230,9 +230,6 @@
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    XDDlibCaptureViewController *vc = [[XDDlibCaptureViewController alloc] init];
-    vc.modalPresentationStyle = UIModalPresentationFullScreen;
-    [self presentViewController:vc animated:NO completion:nil];
     /*if (self.expressionCount == 0) return;
     static NSInteger index = 0;
     index += 1;
